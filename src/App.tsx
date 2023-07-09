e83335dd32cd0cb4bae8d0c057f0a2872c0a4e10
Lucas Alves <lucasalves94@gmail.com>
2023-07-09 18:16:42 -0300

    added header component with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Header } from './components/Header/index';
+import { GlobalStyles } from './styles/GlobalStyles';
+
 export function App() {
-  return <h1> Hello world </h1>
+  return (
+    <>
+    <GlobalStyles />
+    <Header />
+    </>
+  )
 }
